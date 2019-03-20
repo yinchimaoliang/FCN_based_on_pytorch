7c1025de933425e15f45d76c16c071e34f67283b
Yinhao Li <liyinhao0413@qq.com>
2019-03-20 20:08:04 +0800

    finish img_transforms

--- FCN.py
+++ FCN.py
@@ -57,6 +57,8 @@ class Main():
         label = [os.path.join(root, 'SegmentationClass', i + '.png') for i in images]
         return data, label
 
+
+    #选取固定区域
     def rand_crop(self,data,label,height,width):
         data = tfs.CenterCrop((height, width))(data)
         data.show()
@@ -65,13 +67,27 @@ class Main():
         # label = tfs.FixedCrop(*rect)(label)
         return data, label
 
+    def img_transforms(self,im,label,height,width):
+        im,label = self.rand_crop(im,label,height,width)
+        im_tfs = tfs.Compose([
+            tfs.ToTensor(),#[0-255]--->[0-1]
+            tfs.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#均值方差
+        ])
+        im = im_tfs(im)
+        label = self.image2label(label)
+        label = torch.from_numpy(label)
+        return im,label
+
     def main(self):
         data,label = self.loadImage(train = True)
-        img1 = Image.open(data[0])
+        img1 = Image.open(data[0]).convert("RGB")
         img2 = Image.open(label[0]).convert('RGB')
+        im,label = self.img_transforms(img1,img2,HEIGHT,WIDTH)
+        print(im)
+        print(label)
         # label = self.image2label(img2)
-        label = self.image2label(img2)
-        print(label[100:160, 240:250])
+        # label = self.image2label(img2)
+        # print(label[100:160, 200:250])
         # data,label = self.rand_crop(img1,img2,200,300)
         # # data.show()
         # # label.show()
